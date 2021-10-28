a23f31fdce2918f830834740d049072a078e5a5e
sriram <sriramna18102@it.ssn.edu.in>
2021-10-28 22:17:47 +0530

    rssflutter

---
+++ rssfeedflutter/android/app/src/main/kotlin/com/example/rssfeedflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rssfeedflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rssfeedflutter/lib/news_item.dart
@@ -0,0 +1,4 @@
+class NewsItem {
+  String title, description;
+  NewsItem({required this.title, required this.description});
+}
