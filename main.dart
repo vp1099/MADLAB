118b1eea0cb4a2618cad25abe82372c01796f198
vishwaa13 <52492038+vishwaa13@users.noreply.github.com>
2021-10-29 19:14:37 +0530

    Add files via upload
    Only main.dart code to be copied

---
+++ main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+// import 'traffic.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "SSN Workshop",
+    home: Traffic(), // pass the name of the class, which will be the initial class flutter will load
+  ));
+}
+
+class Traffic extends StatelessWidget {
+  const Traffic({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('Traffic Lights'),
+            backgroundColor: Colors.lightBlueAccent),
+        body:Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,color: Colors.blue
+                        ),
+                      )
+                    ]
+                ),
+                Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,color: Colors.blue
+                        ),
+                      )
+                    ]
+                ),
+                Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,color: Colors.blue
+                        ),
+                      )
+                    ]
+                ),
+                Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+                    ]
+                ),
+                Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue
+                        ),
+                      ),
+                    ]
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
